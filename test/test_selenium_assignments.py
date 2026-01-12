16875e4d7ca65a10d29b748241a6df1299fa0465
Tomislav Vuckovic <vuckovic.tomislav1@gmail.com>
2026-01-12 14:38:00 +0100

    test: update driver function with additional options

--- test/test_selenium_assignments.py
+++ test/test_selenium_assignments.py
@@ -8,38 +8,38 @@ from selenium.webdriver.support.select import Select
 @pytest.fixture
 def driver():
     options = webdriver.ChromeOptions()
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
     if os.getenv("CI") == "true":
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
         options.add_argument("--window-size=1920x1080")
-    driver = webdriver.Chrome(options)
-
-    driver.get("https://www.kimmoahola.net/selenium.html")
+    driver = webdriver.Chrome(options=options)
 
     yield driver
 
     driver.quit()
 
 def test_print_main_title_text(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     main_title = driver.find_element(By.ID, "main-title")
     print(f"\nMain Title text: {main_title.text}")
     assert main_title.text == "Selenium Test Page"
 
 def test_locate_paragraph_print_text(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     text_paragraph = driver.find_element(By.CSS_SELECTOR, ".text-paragraph")
     print(f"\nParagraph text: {text_paragraph.text}")
     assert text_paragraph.text == "This paragraph has a class."
 
 def test_input_field_enter_text(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     input_field = driver.find_element(By.NAME, "username")
     input_field.send_keys("igloo")
     print(f"\nInput field value: {input_field.get_attribute('value')}")
     assert input_field.get_attribute("value") == "igloo"
 
 def test_checkbox(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     checkbox2 = driver.find_element(By.NAME, "check2")
     checkbox2.click()
 
@@ -51,6 +51,7 @@ def test_checkbox(driver):
         assert not checkbox2.is_selected() == True
 
 def test_radio_btn_blue(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     radio_btn = driver.find_element(By.XPATH, "/html/body/div[7]/label[3]/input")
     radio_btn.click()
     if radio_btn.is_selected():
@@ -58,6 +59,7 @@ def test_radio_btn_blue(driver):
         assert radio_btn.is_selected() == True
 
 def test_dropdown_option_3(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     dropdown = driver.find_element(By.ID, "dropdown")
     select = Select(dropdown)
     select.select_by_visible_text("Option 3")
@@ -65,18 +67,22 @@ def test_dropdown_option_3(driver):
     assert dropdown.get_attribute("value") == "option3"
 
 def test_click_class_btn(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     class_btn = driver.find_element(By.CSS_SELECTOR, ".btn-class")
     class_btn.click()
 
 def test_click_link_with_id(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     link = driver.find_element(By.ID, "link-id")
     link.click()
     print("\nLink attribute:", link.get_attribute("href"))
 
 def test_nested_span(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     nested_span = driver.find_element(By.CSS_SELECTOR, "#nested-span")
     print(f"\nNested span text: {nested_span.text}")
 
 def test_image_id(driver):
+    driver.get("https://www.kimmoahola.net/selenium.html")
     image_id = driver.find_element(By.ID, "image-id")
     print(f"\nAlt attribute: {image_id.get_attribute('alt')}")
